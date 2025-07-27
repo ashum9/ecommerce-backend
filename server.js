ad1880ba4725f51c6eed141a3d4d5e8067e70d39
ashum <ashumofficial@gmail.com>
2025-07-27 16:11:13 +0530

    server setup done

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 import express from "express"
+import colors from "colors"
+import morgan from "morgan"
+import cors from "cors"
+import dotenv from "dotenv"
+
+//dotenv config
+dotenv.config()
 
 const app = express()
 
+//middleware
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+//route
 app.get("/" , (req,res) => {
     return res.status(200).send("swagat hai")
 })
 
-const PORT = 6969
+const PORT = process.env.PORT || 9696
 
 app.listen(PORT , () => {
-    console.log(`server runnning on ${PORT}`);
+    console.log(`server runnning on ${PORT}`.bgYellow.black);
     
 })

f62e8783f4c13671bce18b4aa7b27ea82ab7330d
ashum <ashumofficial@gmail.com>
2025-07-27 19:51:42 +0530

    user schema added

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true, "naam daal bhai"]
+    },
+    email : {
+        type : String,
+        required : [true , "mail to dalde!"],
+        unique : [true , "420 nhi , naya mail daal"]
+    },
+    password : {
+        type : String ,
+        required : [true , "bosfk accound chumd jaega!"],
+        minLength : [6 , "6 letters se bada "]
+    },
+    address : {
+        type : String , 
+        required : [true , "location mangta mere ko"]
+    },
+    phone : {
+        type : String ,
+        required : [true , "country code ke sath dalne ka"]
+    },
+    profilePic : {
+        type : String
+    }
+}, {timestamps : true })
+
+export const User = mongoose.model("User" , userSchema)

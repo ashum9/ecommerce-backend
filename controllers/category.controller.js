66ae89baa4a4e774f3e3072a28747d7992a4be57
ashum9 <ashumofficial@gmail.com>
2025-08-01 01:48:49 +0530

    delete category controller

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,5 @@
 import CategoryModel from "../models/category.model.js";
+import productModel from "../models/product.model.js";
 
 export const createCategory = async(req,res) => {
  
@@ -49,6 +50,58 @@ export const getAllCategory = async (req,res) => {
         res.status(500).json({
             success : false,
             message : "error in getting all categories"
+        })
+        
+    }
+
+}
+
+export const deleteCategory = async (req,res) => {
+
+    try {
+        
+        // find the category based on parameter
+        // check in db
+        // delete it
+
+        const categories = await CategoryModel.findById(req.params.id)
+
+        if(!categories){
+            return res.status(400).json({
+                success : false ,
+                message : "category not found"
+            })
+        }
+
+        // wo product ki category undefined kro jinki category same hai categories ki tara
+        // fetch products of the category
+        const product = await productModel.find({category: categories._id})
+
+       for(let i = 0 ; i < product.length ; i++){
+        const prdct = product[i];
+        prdct.category = undefined;
+        await prdct.save()
+       }
+        await categories.deleteOne()
+
+        res.status(200).json({
+            success : true , 
+            message : "deleted category successfully"
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        if(error.name === "CastError"){
+            return res.status(500).json({
+                success : false ,
+                message : "invalid id"
+            })
+        }
+
+        res.status(500).json({
+            success : false ,
+            message : "error in deleting category"
         })
         
     }
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { isAuth } from "../middlewares/auth.middleware.js"
-import { createCategory, getAllCategory } from "../controllers/category.controller.js"
+import { createCategory, deleteCategory, getAllCategory } from "../controllers/category.controller.js"
 
 const router = express.Router()
 
 router.post("/create" , isAuth , createCategory)
 router.get("/get-all" , getAllCategory)
+router.delete("/delete/:id" , isAuth , deleteCategory   )
 
 export default router
